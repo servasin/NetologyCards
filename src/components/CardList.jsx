0716fd31a536286622be863fea1ecd5d1162fea3
Sergei Vasin <vvs221@bk.ru>
2023-09-13 20:29:05 +0300

    fix map in App.jsx

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,19 +1,8 @@
-import Card from "./Card"
-import { Children } from "react"
-
 function CardList({children}) {
 
     return (
-      // <div className="cards">
-      //   {/* {cards.map((card, i) =>
-      //     <Card card={card} key={i}/>
-      //   )}   */}
-      //   {children}
-      // </div>
-      <div className="RowList">
-        {Children.map(children, child =>
-          <Card card={child} />
-        )}
+      <div className="cards">
+        {children}
       </div>
     )
   }
